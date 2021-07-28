58766350b9ff0d3de03d50e4444a3c095e50f2c1
Royal-lobster <srujangs8@gmail.com>
2021-07-28 21:40:37 +0530

    changed elif

--- appletrun/__main__.py
+++ appletrun/__main__.py
@@ -14,14 +14,14 @@ def main():
     try:
         java_version = float(re.search('\"(\d+\.\d+).*\"', subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT).decode('utf-8')).groups()[0])
     except:
-        print('\033[91m' +'[Error] Java not found. Please install java <= 8 and try again.'+ '\x1b[0m')
+        print('\033[91m' +'[ERROR] Java not found. Please install java <= 8 and try again.'+ '\x1b[0m')
         print('\033[94m' +'[SUGGESTION] Install from here: https://www.azul.com/downloads/?package=jdk'+ '\x1b[0m')
         java_version = None
     if java_version != None and java_version >= 8 :
             print('\033[91m' +"[ERROR] Applets Not supported on "+str(java_version)+" version of Java"+ '\x1b[0m')
             print('\033[94m' +"[SUGGESTION] change to java 8 or less"+ '\x1b[0m')
             print('\033[94m' +'[SUGGESTION] Install from here: https://www.azul.com/downloads/?package=jdk'+ '\x1b[0m')
-    else:
+    elif java_version != None:
         user_input_path = sys.argv[-1].strip()
         program_path = os.path.abspath(user_input_path)
         program_file_name = os.path.splitext(os.path.basename(program_path))[0]
@@ -41,7 +41,7 @@ def main():
                 print('\033[94m' + "[RUNNING] Applet Started"+ '\x1b[0m')
                 os.system("appletviewer applet.html")
             else:
-                print('\033[91m' +"[Error] Compilation failed"+ '\x1b[0m')
+                print('\033[91m' +"[ERROR] Compilation failed"+ '\x1b[0m')
 
         else:
             print('\033[91m' + "[ERROR] No file specified"+ '\x1b[0m')
@@ -52,7 +52,7 @@ def main():
             try:
                 os.remove(os.path.splitext(program_path)[0] + ".class")
                 os.remove("applet.html")
-                print('\033[92m' +"[Success] Applet closed successfully"+ '\x1b[0m')
+                print('\033[92m' +"[SUCCESS] Applet closed successfully"+ '\x1b[0m')
             except:
                 pass
             
